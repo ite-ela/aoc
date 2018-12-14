c3819f7705f10f889e7c2f6494709fc838077125
vakio <zkuddy@gmail.com>
2018-12-14 22:19:29 +0100

    Add 2018day01, part02

--- src/bin/2018day01.rs
+++ src/bin/2018day01.rs
@@ -1,12 +1,38 @@
+#[macro_use] extern crate maplit;
+
 mod file;
 
 fn main() {
-    let reader = file::file_reader::FileReader::new("./resources/input/2018day01");
+    println!("part01: {}", part01());
+    println!("part02: {}", part02());
+}
+
+fn reader() -> file::file_reader::FileReader {
+    return file::file_reader::FileReader::new("./resources/input/2018day01");
+}
+
+fn part01() -> i32 {
     let mut sum = 0;
-    for line in reader.lines_iterator() {
+    for line in reader().lines_iterator() {
         sum += line.unwrap()
             .parse::<i32>().unwrap()
     }
 
-    println!("{}", sum)
+    return sum;
+}
+
+fn part02() -> i32 {
+    let mut sum = 0;
+    let mut tree_map = btreemap!{sum => sum};
+    loop {
+        for line in reader().lines_iterator() {
+            sum += line.unwrap()
+                .parse::<i32>().unwrap();
+            if tree_map.contains_key(&sum) {
+                return sum;
+            }
+
+            tree_map.insert(sum, sum);
+        }
+    }
 }
